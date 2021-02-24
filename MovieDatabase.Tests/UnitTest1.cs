a39fe095b2665ab34eb01b440ea0e1a794301e76
Salisbury, Tom <t.salisbury@fugro.com>
2021-02-24 12:56:03 +0000

    Filtering / sorting of movies as per specification

--- MovieDatabase.Tests/UnitTest1.cs
+++ MovieDatabase.Tests/UnitTest1.cs
@@ -1,7 +1,8 @@
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using MovieDatabase;
 using System;
 using System.Text.Json;
+using System.Linq;
 
 namespace MovieDatabase.Tests
 {
@@ -16,11 +17,11 @@ namespace MovieDatabase.Tests
         public void MetadataItemConstructor_SetsPropertiesCorrectly()
         {
             // Act
-            var metadataItem = new MetadataItem(id:          1, 
-                                                movieId:     2, 
-                                                title:       "Test", 
-                                                language:    "AnotherTest", 
-                                                duration:    TimeSpan.Parse("10:10:10"),  
+            var metadataItem = new MetadataItem(id: 1,
+                                                movieId: 2,
+                                                title: "Test",
+                                                language: "AnotherTest",
+                                                duration: TimeSpan.Parse("10:10:10"),
                                                 releaseYear: 2019);
 
             // Assert
@@ -36,7 +37,7 @@ namespace MovieDatabase.Tests
         {
             // Arrange
             const string json = "{\"movieId\": 3, \"title\": \"Elysium\",\"language\": \"EN\",\"duration\": \"1:49:00\",\"releaseYear\": 2013}";
-           
+
             // Act
             var metadataItem = JsonSerializer.Deserialize<MetadataItem>(json, new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
 
@@ -46,5 +47,50 @@ namespace MovieDatabase.Tests
             Assert.AreEqual(metadataItem.Duration, TimeSpan.Parse("1:49:00"));
             Assert.AreEqual(metadataItem.ReleaseYear, 2013);
         }
+
+
+        /// <summary>
+        /// ● Only the latest piece of metadata (highest Id) should be returned where there are multiple metadata records for a given language.
+        /// </summary>
+        [Test]
+        public void MetadataRepositoryGetByMovieId_RetrievesHighestIdForLanguage()
+        {
+            // Arrange
+            var itemUnderTest = new MetadataRepository();
+            itemUnderTest.Movies.AddRange(new[]
+            {
+                new MetadataItem(id:          1,
+                                 movieId:     2,
+                                 title:       "Test",
+                                 language:    "BB",
+                                 duration:    TimeSpan.Parse("10:10:10"),
+                                 releaseYear: 2019),
+
+                new MetadataItem(id:          2,
+                                 movieId:     2,
+                                 title:       "Test",
+                                 language:    "BB",
+                                 duration:    TimeSpan.Parse("10:10:10"),
+                                 releaseYear: 2019),
+
+                new MetadataItem(id:          3,
+                                 movieId:     2,
+                                 title:       "Test",
+                                 language:    "AA",
+                                 duration:    TimeSpan.Parse("10:10:10"),
+                                 releaseYear: 2019),
+            });
+
+            // Act
+            var data = itemUnderTest.GetByMovieId(2).ToArray();
+
+            // Assert
+            Assert.That(data.Length == 2);              // One record (id: 1) should be filtered
+            Assert.That(data[0].Language == "AA");      // We should be sorted by language
+            Assert.That(data[1].Language == "BB");
+            Assert.That(data[1].Id == 2);              // We want the higher Id with language BB
+
+
+         }
     }
 }
--- MovieDatabase/MetadataRepository.cs
+++ MovieDatabase/MetadataRepository.cs
@@ -8,5 +8,29 @@ namespace MovieDatabase
     public class MetadataRepository
     {
         public List<MetadataItem> Movies { get; } = new List<MetadataItem>();
+
+        /// <summary>
+        /// Returns all of the movies for the specified id doing the language filter,,, should this live here.. maybe not
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IEnumerable<MetadataItem> GetByMovieId(ulong id)
+        {
+            // filter all of the movied by id
+            var byId = Movies.Where(f => f.MovieId == id);
+
+            // where valid looking at CSV are there any? may have duff data coming in??
+            var whereValid = byId.Where(f => f.Duration > TimeSpan.Zero &&
+                                             f.ReleaseYear > 0 &&
+                                             !string.IsNullOrEmpty(f.Title) &&
+                                             !string.IsNullOrEmpty(f.Language));
+
+            // We only want the first of each language so group and then select item
+            var groupedByLang = whereValid.GroupBy(f => f.Language, (key, item) => item.OrderBy(f => f.MovieId).Last());
+
+            // Then we want to order by the language
+            return groupedByLang.OrderBy(f => f.Language);
+
+        }
     }
 }
