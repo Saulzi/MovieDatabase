9a8e97bd72a03f626a07c0ec8ba6141504682073
Salisbury, Tom <t.salisbury@fugro.com>
2021-02-24 11:14:10 +0000

    Added Endpoints as per document

---
+++ MovieDatabase/Controllers/MetadataController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieDatabase.Controllers
+{
+    public class MetadataController : ControllerBase
+    {
+        [HttpPost("metadata")]
+        public IActionResult PostMetadata()
+        {
+            return Ok();
+        }
+
+        [HttpGet("metadata/{id}")]
+        public IActionResult GetMetadata(string id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ MovieDatabase/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieDatabase.Controllers
+{
+    [Route("movies")]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet("stats")]
+        public IActionResult GetStats()
+        {
+            return Ok();
+        }
+    }
+}
