ac04a357a32aecb5a368634341d470a77046fa97
Salisbury, Tom <t.salisbury@fugro.com>
2021-02-24 14:09:30 +0000

    Loading CSV now working,,, used some rando regex pulled from StackOverflow as could not be bothered to pull in some Nuget package or whatever..
    Spend a bit over an hour, basics are working, not implemented stats, not going to implement due to time etc.
    
    Hope this shows you what you are after
    
    Tom

--- MovieDatabase/MetadataItem.cs
+++ MovieDatabase/MetadataItem.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MovieDatabase
 {
@@ -22,9 +26,12 @@ namespace MovieDatabase
         public MetadataItem(ulong id, ulong movieId, string title, string language, TimeSpan duration, ushort releaseYear) => (Id, MovieId, Title, Language, Duration, ReleaseYear) = (id, movieId, title, language, duration, releaseYear);
 
         // this is rougth as it doesn't handle CSV being different way round etc.... this also is not very defensive but will do here for the moment
-        public static MetadataItem FromCSV(string CSV)
+        public static MetadataItem FromCSV(string csv)
         {
-            var items = CSV.Split(",");
+            // ".+?"|[^"]+?(?=,)|(?<=,)[^"]+
+
+
+            var items = SplitCSV(csv).ToArray();
 
             // Would be nice to deconstruct ala javascript on array (id, movieId, title, language, duration, releaseYear) = items
             var id = ulong.Parse(items[0]);
@@ -35,6 +42,17 @@ namespace MovieDatabase
             var releaseYear = ushort.Parse(items[5]);
 
             return new MetadataItem(id, movieId, title, language, duration, releaseYear);
+        }
+
+        // https://stackoverflow.com/questions/3776458/split-a-comma-separated-string-with-both-quoted-and-unquoted-strings/23888636
+        private static IEnumerable<string> SplitCSV(string input)
+        {
+            Regex csvSplit = new Regex("(?:^|,)(\"(?:[^\"]+|\"\")*\"|[^,]*)", RegexOptions.Compiled);
+
+            foreach (Match match in csvSplit.Matches(input))
+            {
+                yield return match.Value.TrimStart(',');
+            }
         }
 
     }
